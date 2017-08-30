bbde99ca6de66d70d3e1dfbd7b2af4853c306d90
Trent Robbins <robbintt@gmail.com>
2017-08-30 16:59:39 -0700

    Added webapp path to root path.

--- hackersinresidence/webapp/views.py
+++ hackersinresidence/webapp/views.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    ''' test view
+    '''
+    return HttpResponse("Test view")
