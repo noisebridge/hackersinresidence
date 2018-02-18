88f2526f41c4be49b7e11f2466ac20cb62d8df5f
Trent Robbins <robbintt@gmail.com>
2018-02-18 00:39:30 -0800

    Added custom text for residency opportunity form

--- hackersinresidence/webapp/forms.py
+++ hackersinresidence/webapp/forms.py
@@ -71,8 +71,44 @@ class OpportunityForm(forms.ModelForm):
                 }
         # use regex to copy the dict keys above and add manual titles
         labels = {
-                'title': ('Opportunity Title')
-                }
+                     'title': ('Create a new Residency Opportunity'),
+                     'description': ('Title of this Opportunity'),
+                     'expiration_date': ('Describe this Residency Opportunity'),
+                     'location_city': ('City of the Residency Opportunity'),
+                     'location_country': ('Country of the Residency Opportunity'),
+                     'offer_travel_checkbox': ('Check this box if you offer travel expenses'),
+                     'offer_travel_detail': ('Offer Travel Detail'),
+                     'offer_housing_checkbox': ('Check this box if you offer housing'),
+                     'offer_housing_detail': ('Give details of housing offered'),
+                     'offer_food_checkbox': ('Check this box if you offer food'),
+                     'offer_food_detail': ('Give details of food offered'),
+                     'offer_stipend_checkbox': ('Check this box if you offer a stipend'),
+                     'offer_stipend_detail': ('Give details of stipend offered'),
+                     'offer_studio_checkbox': ('Check this box if you offer studio space'),
+                     'offer_studio_detail': ('Give details of studio space offered'),
+                     'offer_tools_checkbox': ('Check this box if you offer use of tools'),
+                     'offer_tools_detail': ('Give details of tools offered'),
+                     'offer_additional_detail': ('If there is anything else you offer for this Residency, describe it here'),
+                     'require_language': ('List languages and skill levels required'),
+                     'require_start_date': ('Earliest start date of this Residency Opportunity'),
+                     'require_end_date': ('Latest end date of this Residency Opportunity'),
+                     'require_minimum_stay': ('Minimum time required for this Residency'),
+                     'require_maximum_stay': ('Maximum time of stay for this Residency'),
+                     'require_date_detail': ('If needed, give details about the length of stay'),
+                     'require_mentoring_checkbox': ('Check this box if the resident is required to act as a mentor for members of your community'),
+                     'require_mentoring_detail': ('Give details about mentoring required'),
+                     'require_talk_checkbox': ('Check this box if the resident is required to give a talk'),
+                     'require_talk_detail': ('Give details about talk(s) the Resident is required to give'),
+                     'require_workshop_checkbox': ('Check this box if the resident is required to give a workshop'),
+                     'require_workshop_detail': ('Give details about workshop(s) the Resident is required to give'),
+                     'require_presentation_checkbox': ('Check this box if the Resident is required to give a presentation'),
+                     'require_presentation_detail': ('Give details about presentation(s) the Resident is required to give'),
+                     'require_class_checkbox': ('Check this box if the Resident is required to give a class'),
+                     'require_class_detail': ('Give details about class(es) the Resident is required to give'),
+                     'require_hackathon_checkbox': ('Check this box if the Resident is required to organize a hackathon'),
+                     'require_hackathon_detail': ('Give details about hackathon(s) the Resident is required to give'),
+                     'require_other_requirements': ('If there is anything else you require for this Residency, describe it here'),
+                    }
 
 class OrganizationForm(forms.ModelForm):
     ''' Django form describing the Opportunity model
